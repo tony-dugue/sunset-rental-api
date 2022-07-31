c8f2ad47a4beacf3115f1f74254cd345553facc5
tony dugué <duguetony@gmail.com>
2022-07-31 11:57:47 +0200

    initialization apollo GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: (process.env.NODE_ENV === 'development' && true) || false,
+      debug: (process.env.NODE_ENV === 'development' && true) || false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
